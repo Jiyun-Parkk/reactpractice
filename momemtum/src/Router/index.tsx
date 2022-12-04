16a4303722db8027ab3293fa856fad0175bd181a
박지윤 <jyun1937@naver.com>
2022-12-04 21:00:35 +0900

    ADD: momemtum page add

--- momemtum/src/Router/index.tsx
+++ momemtum/src/Router/index.tsx
@@ -1,10 +1,11 @@
-import { Login, SignUp } from 'page'
+import { Login, Momemtum, SignUp } from 'page'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const Router = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
+        <Route path='/' element={<Momemtum />} />
         <Route path='/signin' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
       </Routes>
---
+++ momemtum/src/page/Momemtum/index.tsx
@@ -0,0 +1,3 @@
+export const Momemtum = () => {
+  return <></>
+}
